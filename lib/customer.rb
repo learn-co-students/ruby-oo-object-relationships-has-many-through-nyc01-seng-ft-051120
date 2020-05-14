6ca1900dd7bceee1caade44879fcccef51cecf46
callas4ever <thetseringnorbu@gmail.com>
2020-05-14 19:42:33 -0400

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,38 @@
+require 'pry'
+
 class Customer
-  
+  attr_accessor :meal, :waiter
+  @@all=[]
+  def initialize(name, age)
+    @age=age
+    @name=name
+    @@all<<self
+    @waiter=waiter
+
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_meal(waiter, total, tips)
+    Meal.new(waiter,self,total, tips)
+  end
+
+  def meals
+    Meal.all.select do |meal|
+        meal.customer==self
+    end
+    end
+
+    def waiters 
+        mean=[]
+       Meal.all.each do |meal|
+            if meal.customer==self
+              mean << meal.waiter
+            end  
+        end
+        mean
+    end
+
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,38 @@
 class Waiter
+    @@all=[]
+attr_accessor :meal
+def initialize(name, year_of_experience)
+    @name=name
+    @year_of_experience=year_of_experience
+    @@all<<self
+end
+def self.all
+    @@all
+end
 
+def new_meal(customer, total, tips)
+    Meal.new(self, customer, total, tips)
+end
+def meals
+    Meal.all.select do |meal|
+        meal.waiter==self
+    end
+end
+def best_tipper
+    min=0
+    best=nil
+    Meal.all.each do |meal|
+        if meal.waiter==self && meal.tips>min
+            min=meal.tips   
+    end
+end
+Meal.all.each do |meal|
+if meal.tips==min
+best=meal.customer
+end
+            
+
+end
+best
+end
 end
