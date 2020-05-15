fdd45ff32d50b8acaf0e6fd627867b50acc0b30d
Ideal-Poster <malcolmgourdine@protonmail.com>
2020-05-15 00:10:50 -0400

    Done.

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,26 @@
+require 'pry'
 class Waiter
+  @@all = []
+  def initialize(name, years_of_experience)
+    @@all << self
+  end
 
+  def self.all
+    @@all
+  end
+
+  def new_meal(customer, total, tip=0)
+    Meal.new(self, customer, total, tip=0)
+  end
+
+  def meals
+    Meal.all.select { |meal| meal.waiter == self }
+  end
+
+  def best_tipper
+    meals
+      .sort_by { |meal| meal.tip }
+      .last
+      .customer
+  end
 end
