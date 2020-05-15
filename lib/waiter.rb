24860f893ac57603169ed3063cbc663d860b893a
ChayaGreisman <chayamgreisman@gmail.com>
2020-05-14 21:05:02 -0700

    Done.

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -24,13 +24,6 @@ class Waiter
         end
     end
 
-
-    def waiters
-        meals.map do |meal|
-          meal.waiter
-        end
-    end
-
     def best_tipper
         best_tipped_meal = meals.max do |meal_a, meal_b|
           meal_a.tip <=> meal_b.tip
